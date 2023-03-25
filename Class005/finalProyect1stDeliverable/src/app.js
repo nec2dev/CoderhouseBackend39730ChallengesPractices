efc3313fe266b988daeae7e829a6011a3af3d654
nec2dev <nec2multivackergithub@outlook.es>
2023-03-25 19:45:51 -0300

    FinalProject1stDeliverable ok

---
+++ Class005/finalProyect1stDeliverable/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import productRouter from './routes/products.router.js';
+import cartRouter from './routes/carts.router.js';
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/api/products' , productRouter);
+app.use('/api/carts' , cartRouter);
+
+const server = app.listen(PORT, () => console.log("Server up on port: " + PORT));
+
+
